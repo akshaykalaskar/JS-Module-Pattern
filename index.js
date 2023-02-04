b0a254275e427422f24f27828e6454fc1490a50a
akshaykalaskar <akshaykalaskar@hotmail.com>
2023-02-04 23:49:30 +0000

    boiler plate created

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-// Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Module Pattern</h1>`;
+const moduleController = (() => {
+  // All business logic here..
+  let num = 10;
+
+  let addition = (outerValue) => {
+    return outerValue + num;
+  };
+
+  let showResult = (outerValue) => {
+    return addition(outerValue);
+  };
+
+  return {
+    showResult: showResult,
+  };
+})();
+
+console.log(moduleController.showResult(5));
+
+const viewController = (() => {
+  // All UI modification/ rendering will comer here.
+  return {};
+})();
+
+const eventsController = ((moduleCtrl, viewCtrl) => {
+  // All events logic here..
+  const result = moduleCtrl.showResult(11);
+
+  const getResult = () => {
+    return result;
+  };
+
+  return {
+    getResult: getResult,
+  };
+})(moduleController, viewController);
+
+console.log('Calling other cntrl value', eventsController.getResult());
